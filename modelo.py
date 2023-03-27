44602ec45067339cefa425096ff246336c4531a7
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-03-27 16:19:54 -0300

    part 4

--- modelo.py
+++ modelo.py
@@ -20,24 +20,35 @@ class Programa:
     def dar_likes(self):
         self._likes += 1
 
+    def imprime(self):
+        print(f'Nome: {self.nome} - Ano: {self.ano} - {self.likes} Likes')
+
+
 #Definir minha classe mãe que é o "Programa", assim podemos compartilhar tudo q estiver na classe mãe para as outras classe que sejam filhas.
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'Nome: {self.nome} - Ano: {self.ano} - Duração 🕒: {self.duracao} min - {self.likes} Likes 👍'
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano) #chamar a Classe mãe
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'Nome: {self.nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - {self.likes} Likes 👍'
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_likes()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
-
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporada: {atlanta.temporadas} - Likes: {atlanta.likes}')
 
+filmes_e_series = [vingadores, atlanta]
+
+for Programa in filmes_e_series:
+    print(Programa)
