307034773947e51ca7cbd73790a70e592f47c601
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-04-01 20:05:44 -0300

    part 5

--- modelo.py
+++ modelo.py
@@ -44,7 +44,17 @@ class Serie(Programa):
 class Playlist(list):
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -65,8 +75,10 @@ atlanta.dar_likes()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
 
-for programas in playlist_fim_de_semana:
-    print(programas)
+len(playlist_fim_de_semana)
+
+for programa in playlist_fim_de_semana:
+    print(programa)
 
